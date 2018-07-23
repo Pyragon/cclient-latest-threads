0e5f5eddcc82a2de44d736ccc668e55b6a732744
Pyragon <eldo.imo.rs@hotmail.com>
2018-07-23 00:14:48 -0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,94 @@
+const dateFormat = require('dateformat');
+
+var latest = function(main) {
+
+  var threads = [];
+  var error;
+
+  return {
+
+    init: (config) => {},
+
+    getDom: () => {
+      console.log('getting dom ' + threads);
+      var container = document.createElement('div');
+
+      var title = document.createElement('p');
+      title.id = 'latest-title';
+      title.innerHTML = 'Latest Threads';
+
+      container.appendChild(title);
+
+      if (threads.length == 0) {
+        var loader = document.createElement('div');
+        loader.id = 'latest-loader';
+        var spinner = document.createElement('i');
+        spinner.className = 'fa fa-spinner fa-spin';
+
+        loader.appendChild(spinner);
+        container.appendChild(loader);
+        return container;
+      }
+
+      for (var i = 0; i < threads.length; i++) {
+        var thread = threads[i];
+        console.log(thread);
+
+        var div = document.createElement('div');
+        div.className = 'news-post';
+
+        var thread_title = document.createElement('p');
+        thread_title.className = 'news-title';
+        thread_title.innerHTML = thread.subject;
+
+        var author = document.createElement('p');
+        author.className = 'news-author';
+        var authorline = 'Posted ';
+        authorline += dateFormat(new Date(thread.dateline * 1000), 'mmmm dS, yyyy');
+        authorline += ' By ' + thread.formattedName;
+        author.innerHTML = authorline;
+
+        div.appendChild(thread_title);
+        div.appendChild(author);
+
+        container.appendChild(div);
+        console.log(container);
+      }
+
+      return container;
+    },
+
+    update: () => {
+      var plugin = this;
+      main.request({
+        path: '/forums/posts',
+        method: 'GET'
+      }, {
+        filter: 'latest',
+        limit: 3
+      }, (response) => {
+        if (response.error) {
+          console.error(response.error);
+          return;
+        }
+        threads = response.threads;
+        main.getPluginManager().updateDom('latest-threads-plugin');
+      });
+    },
+
+    getName: () => {
+      return "latest-threads-plugin";
+    },
+
+    getDelay: () => {
+      return 30000;
+    },
+
+    getStylesheets: () => {
+      return ['styles/style.css', '/styles/fa/css/all.min.css'];
+    }
+
+  };
+
+};
+module.exports = latest;
